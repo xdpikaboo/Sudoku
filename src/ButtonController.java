22cc6030193e42732dcb3259445af9bd8497a0dd
xdpikaboo <xdpikaboo@gmail.com>
2022-02-22 17:49:46 -0500

    Code Upload

---
+++ src/ButtonController.java
@@ -0,0 +1,40 @@
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JCheckBox;
+
+/**
+ * This class controls all user actions from ButtonPanel.
+ *
+ */
+public class ButtonController implements ActionListener {
+    private Game game;
+
+    /**
+     * Constructor, sets game.
+     *
+     * @param game  Game to be set.
+     */
+    public ButtonController(Game game) {
+        this.game = game;
+    }
+
+    /**
+     * Performs action after user pressed button.
+     *
+     * @param e ActionEvent.
+     */
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("New"))
+            game.newGame();
+        else if (e.getActionCommand().equals("Check"))
+            game.checkGame();
+        else if (e.getActionCommand().equals("Exit"))
+            System.exit(0);
+        else if (e.getActionCommand().equals("Help on"))
+            game.setHelp(((JCheckBox)e.getSource()).isSelected());
+        else
+            game.setSelectedNumber(Integer.parseInt(e.getActionCommand()));
+    }
+}
---
+++ src/UpdateAction.java
@@ -0,0 +1,8 @@
+
+public enum UpdateAction {
+    NEW_GAME,
+    CHECK,
+    SELECTED_NUMBER,
+    CANDIDATES,
+    HELP
+}
